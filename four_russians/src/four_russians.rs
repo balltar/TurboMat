873b62b7e00013546574ec3ae08b0d52204d7a58
balltar <trball13@gmail.com>
2022-05-19 12:13:00 -0400

    rank computation works!

--- four_russians/src/four_russians.rs
+++ four_russians/src/four_russians.rs
@@ -9,7 +9,7 @@ pub fn print_type_of<T>(_: &T) { println!("{}", std::any::type_name::<T>()) }
 pub fn ceil_div(x: Index, y: Index) -> Index { x / y + (x % y != 0) as Index }
 pub fn ceil_rem(x: Index, y: Index) -> Index { if x % y != 0 {x % y} else {y} }
 
-const fn log_2(x: usize) -> usize {
+pub const fn log_2(x: usize) -> usize {
     assert!(x > 0);
     WORD_SIZE -  (x.leading_zeros() as usize) - 1
 }
@@ -17,6 +17,12 @@ const fn log_2(x: usize) -> usize {
 #[derive(Debug)]
 pub struct Matrix<T, const M: usize, const N: usize> {
     pub rows: [[T; N]; M]
+}
+
+impl<T, const M: Index, const N: Index> Matrix<T, M, N> {
+    pub fn num_rows(&self)   -> u32 { M.try_into().unwrap() }
+    pub fn num_cols(&self)   -> u32 { N.try_into().unwrap() }
+    pub fn dimensions(&self) -> (u32, u32) { (M.try_into().unwrap(), N.try_into().unwrap()) }
 }
 
 impl<const M: Index, const N: Index> Matrix<Word, M, N> {
@@ -50,30 +56,33 @@ trait UpdateRow<const N: Index> {
 
 impl<const N: Index> Bit for Row<N> {
     /// Determine if a row has nonzero entry at a given bit index.
+    /// We assume bit indexing follows little endian representation of a word.
+    //  NOTE: bit shift operations are endianess agnostic in languages
+    //  like C and Rust. This means >> shifts towards the LEAST SIGNIFICANT
+    //  bit and << shifts towards the MOST SIGNIFICANT bit regardless of
+    //  endianess convention used (e.g., little endianess used for all Intel
+    //  processors and big endian often used for web applications).
     fn bit(&self, bit_index: Index) -> bool {
         assert!(bit_index / WORD_SIZE < N);
-        let probe: Index = 1 << (bit_index % WORD_SIZE);
+        let probe: Index = 1 << bit_index % WORD_SIZE;
         self[bit_index / WORD_SIZE] & probe != 0 
     }
 
     /// Return the bits from bit_start to bit_start + width
+    /// We assume bit indexing follows little endian representation of a word.
+    //  NOTE: bit shift operations are endianess agnostic in languages
+    //  like C and Rust. This means >> shifts towards the LEAST SIGNIFICANT
+    //  bit and << shifts towards the MOST SIGNIFICANT bit regardless of
+    //  endianess convention used (e.g., little endianess used for all Intel
+    //  processors and big endian often used for web applications).
     fn bits(&self, bit_row_start: Index, width: Index) -> Word {
-        assert!(if bit_row_start + width > WORD_SIZE {N > bit_row_start / WORD_SIZE} else {true});
         let w0: Index = bit_row_start / WORD_SIZE;
         let w1: Index = (bit_row_start + width - 1) / WORD_SIZE;
         let bit_word_start: Index = bit_row_start % WORD_SIZE;
 
-        let mut result: Word = self[w0];
+        let mut result: Word = self[w0] >> bit_word_start;
         
-        if w0 < w1 {
-            result <<= width - (WORD_SIZE - bit_word_start);
-            result  ^= w1 >> (WORD_SIZE - (width - (WORD_SIZE - bit_word_start)));
-        }
-        else {
-            result >>= WORD_SIZE - (bit_word_start + width);
-        }
-
-        
+        if w0 < w1 { result  ^= self[w1] << WORD_SIZE - bit_word_start; }
         result & ((1 << width) - 1)
     }
 }
@@ -94,50 +103,29 @@ pub fn print_mat<T: std::fmt::Debug + Copy, const M: Index, const N: Index>(mat:
 
 pub fn print_gf2_mat<const M: Index, const N: Index>(mat: &Matrix<Word, {M}, {N}>) {
     for row in mat.rows {
-        print!("|");
+        print!("[ ");
         for word in row {
-            //print!("{:0>width$}|", format!("{:b}", word), width = WORD_SIZE);
-            let s = format!("{:0>width$}|", format!("{:b}", word), width = WORD_SIZE);
-            print!("{}", s);
+            // :b displays words in big endian order
+            let s = format!(" {:0>width$}", format!("{:b}", word), width = WORD_SIZE);
+            // Display bits in little endian order
+            print!("{}", &s.chars().rev().collect::<String>());
         }
-        print!("\n");
+        print!("]\n");
     }
 }
-
-/* 
-     This diagram summarises the method. We use "4 Russian" tables
-     of width S bits (S=3 below); current block starts at column s;
-     to find pivot for row r, we start at j=r and reduce to the
-     left using pivots s .. r-1; then check whether row j has a
-     pivot for row r -- if it does, (possibly) xor it onto row r.
-     Once the block is complete, form the table of size 2^S and
-     reduce below...
-         +-----+--------------------------------+
-         |1 * *|* * * * * * * * * * * * * * * * |
-         |  1 *|* * * * * * * * * * * * * * * * |
-         |    1|* * * * * * * * * * * * * * * * |
-         +-----+-----+--------------------------+
-     s-> |     |1 * *|* * * * * * * * * * * * * |
-         |     |0 0 *|* * * * * * * * * * * * * | <- r
-         |     |0 0 *|* * * * * * * * * * * * * |
-         |     |* * *|* * * * * * * * * * * * * | <- j
-         |     |* * *|* * * * * * * * * * * * * |
-         |     |* * *|* * * * * * * * * * * * * |
-         +--------------------------------------+
-      On failure to find a pivot, break out for a simpler loop
-      that finishes it off.
-*/
 
 pub fn four_russians_mat_print<const M: Index, const N: Index>(bits_per_chunk: Index, mat: &Matrix<Word, {M}, {N}>) {
     for row in mat.rows {
         let mut row_string = String::new();
         for word in row {
+            // :b displays words in big endian order
             let zeropadded_bit_word = &format!("{:0>width$}", format!("{:b}", word), width = WORD_SIZE);
-            row_string.push_str(zeropadded_bit_word);
+            // Display bits in little endian order
+            row_string.push_str(&zeropadded_bit_word.chars().rev().collect::<String>());
         }
         let row_chars = row_string.chars();
         let mut i = 1;
-        print!("[");
+        print!("[ ");
         for c in row_chars {
             print!("{}", c);
             if i % WORD_SIZE == 0 {
@@ -152,88 +140,88 @@ pub fn four_russians_mat_print<const M: Index, const N: Index>(bits_per_chunk: I
     }
 }
 
-
 const WORD_SIZE: usize = std::mem::size_of::<usize>() * 8;
 
-/*
+/* 
+     This diagram summarises the method. We use "4 Russian" tables
+     of width bits_per_chunk bits (bits_per_chunk=3 below); current block starts at column chunk_start;
+     to find pivot for row row_index, we start at j=row_index and reduce to the
+     left using pivots chunk_start .. row_index-1; then check whether row j has a
+     pivot for row row_index -- if it does, (possibly) xor it onto row row_index.
+     Once the block is complete, form the table of size 2^bits_per_chunk and
+     reduce below...
+                   +-----+--------------------------------+
+                   |1 * *|* * * * * * * * * * * * * * * * |
+                   |  1 *|* * * * * * * * * * * * * * * * |
+                   |    1|* * * * * * * * * * * * * * * * |
+                   +-----+-----+--------------------------+
+     chunk_start-> |     |1 * *|* * * * * * * * * * * * * |
+                   |     |0 0 *|* * * * * * * * * * * * * | <- row_index
+                   |     |0 0 *|* * * * * * * * * * * * * |
+                   |     |* * *|* * * * * * * * * * * * * | <- j
+                   |     |* * *|* * * * * * * * * * * * * |
+                   |     |* * *|* * * * * * * * * * * * * |
+                   +--------------------------------------+
+      On failure to find a pivot, break out for a simpler loop
+      that finishes it off.
+*/
+
 pub fn four_russians_rank<const M: Index, const N: Index>(mat: &mut Matrix<Word, {M}, {N}>) -> u32 {
-    const a: usize = Matrix::<Word, {M}, {N}>::N_words;
-    const b: usize = log_2(a);
+    /*
+     * Unfortunately, Rust is kind of dumb with the way it handles
+     * constant generic types... you can't have bits_per_row below
+     * be a const... this creates a headache for the lookup table!
+     */
+    let bits_per_row:   Index = N * WORD_SIZE;
+    let bits_per_chunk: usize = log_2(M); //If bits_per_chunk <= 1 then error?
 
-    let mut Z = Matrix::<usize, b, N> {
-        rows: [[0; N]; b]
-    };
-
-    return 5;
-}*/
-
-
-pub fn four_russians_rank<const M: Index, const N: Index>(mat: &mut Matrix<Word, {M}, {N}>) -> u32 {
-    // Unfortunately, Rust is kind of dumb with the way it handles
-    // constant generic types... you can't have bits_per_row below
-    // be a const... this creates a headache for the lookup table!
-    let bits_per_row: Index   = N * WORD_SIZE;
-    let bits_per_chunk: usize = log_2(bits_per_row);
-
-    // Initialize the Four Russians Lookup Table.
-    // It will store all linear combinations of the pivots we have found
-    // thus far. Since there are bits_per_chunk many pivots and each
-    // can have 0 or 1 coefficient, there will be 2^bits_per_chunk many
-    // entries needed for the ith chunk.
-    // There are N chunks, so this is how we get Z below.
+    /*
+     * Initialize the Four Russians Lookup Table.
+     * It will store all linear combinations of the pivots we have found
+     * thus far. Since there are bits_per_chunk many pivots and each
+     * can have 0 or 1 coefficient, there will be 2^bits_per_chunk many
+     * entries needed for the ith chunk.
+     * There are N chunks, so this is how we get Z below.
+     */
     let lookup_table_size: usize = 1 << bits_per_chunk;
-    let mut z: Vec<Word>      = Vec::with_capacity(lookup_table_size);
-    let mut Z: Vec<Row<N>> = Vec::with_capacity(lookup_table_size); 
-
-    //println!("The number of bits per row is {:?}", bits_per_row);
-    //println!("The number of bits per chunk is {:?}", bits_per_chunk);
-    //println!("The number of chunks is {:?}", ceil_div(bits_per_row, bits_per_chunk));
-    //println!("The number of bits in the last chunk is: {:?}", ceil_rem(bits_per_row, bits_per_chunk));
+    let mut lookup_indices: Vec<Word>   = vec![     0; lookup_table_size];
+    let mut lookup_table:   Vec<Row<N>> = vec![[0; N]; lookup_table_size]; 
     
     // Variable rank will store the rank of mat.
-    let row_index: Index = 0;
+    let mut row_index: Index = 0;
 
-    // Variable chunk_start will store the starting index of the current chunk
-    let mut chunk_start: Index = 0;
-
-    while bits_per_chunk > 1 && chunk_start + bits_per_chunk <= M && row_index == chunk_start {
+    // Randomized Four Russian Method until not a full chunk or no pivot found
+    'bigloop: for chunk_start in (0..M-M % bits_per_chunk).step_by(bits_per_chunk) {
         // Seach for pivots for this chunk
         for _ in chunk_start..chunk_start + bits_per_chunk {
             // Search the remaining rows for a pivot
-            let mut j: Index = row_index;
-            while j < M {
+            let mut found_pivot = None;
+            for j in row_index..M {
                 for k in chunk_start..row_index {
                     // Reduce relative to this chunk using pivots found so far
                     if mat.rows[j].bit(k) {
-                        mat.add_rows(j, k, chunk_start / WORD_SIZE, N)
+                        mat.add_rows(j, k, chunk_start / WORD_SIZE, N);
                     }
                 }
                 // Check to see if we found a pivot! If so, stop the search!
-                if mat.rows[j].bit(row_index) { break; }
-
                 // Otherwise, try the next row for a pivot!
-                j += 1;
+                if mat.rows[j].bit(row_index) { 
+                    found_pivot = Some(j);
+                    break; 
+                }
             }
-            
-            // We've reached the end
-            if j == M { break; }
 
-            // Create a new pivot
-            if j != row_index { mat.add_rows(row_index, j, chunk_start / WORD_SIZE, N); }
-
-            //if piv thing
+            match found_pivot {
+                None                      => break 'bigloop,
+                Some(j) if j != row_index => mat.add_rows(row_index, j, chunk_start / WORD_SIZE, N),
+                _                         => (),
+            }
+            row_index += 1;
         }
-
-        // Couldn't find enough pivots
-        if row_index != chunk_start + bits_per_chunk { break; }
-
-        // End of matrix
+        // If not at end of matrix, then proceed with reduction
         if chunk_start + bits_per_chunk == M { break; }
 
-        //Clear z[0] to start over for this chunk.
-        z[0] = 0;
-
-        // For each pivot
+        // Construct the four russians lookup table with pivots
         for i in 0..bits_per_chunk {
             // If we index the pivots 0..bits_per_chunk, then 
             // ii counts the number of linear combinations of 
@@ -241,35 +229,60 @@ pub fn four_russians_rank<const M: Index, const N: Index>(mat: &mut Matrix<Word,
             // coefficient being i, i.e., 2^i.
             let ii = 1 << i;
 
-            // Let's grab the bits for the i^th pivot!
+            // Grab the bits from the i^th pivot!
             let vv = mat.rows[chunk_start+i].bits(chunk_start, bits_per_chunk);
 
-            // For each of our previously found linear combinations we 
-            // xor with our new pivot.
+            // For each of our previously found linear combinations we xor with our new pivot.
             for j in 0..ii {
-                let a = z[j];
-                let b = a ^ vv;
-                z[j+ii] = b;
-                // is following line necessary?
-                let Za  = Z[a];
-                Z[b].update_add_rows(&Za, &mat.rows[chunk_start+i], bits_per_chunk / WORD_SIZE, N)
-            }
-
-            // Now reduce below the pivots!
-            for i in bits_per_chunk + chunk_start .. M {
-                let c = mat.rows[i].bits(chunk_start, bits_per_chunk);
-                mat.update_xor_row(i, &mut Z[c], bits_per_chunk / WORD_SIZE, N)
+                let a   = lookup_indices[j];
+                let b   = a ^ vv;
+                lookup_indices[j+ii] = b;
+                let Za  = lookup_table[a];
+                lookup_table[b].update_add_rows(&Za, &mat.rows[chunk_start+i], bits_per_chunk / WORD_SIZE, N)
             }
         }
-
-        // NEXT TIME: WHY IS THIS NEVER REACHED??
-        print_gf2_mat(&mat);
-
-        // Next chunk!
-        chunk_start += bits_per_chunk;
+        // Now reduce below the pivots!
+        for i in chunk_start + bits_per_chunk .. M {
+            let c = mat.rows[i].bits(chunk_start, bits_per_chunk);
+            mat.update_xor_row(i, &mut lookup_table[c], bits_per_chunk / WORD_SIZE, N)
+        }
     }
 
+    // Brute force the rest of the way!
+    // Traverse columns one after another in search of a pivot!
+    let mut col_index: Index = row_index; 
+    while row_index < M && col_index < bits_per_row {
+        let mut pivot = None;
+        for j in row_index..M {
+            if mat.rows[j].bit(col_index) { 
+                pivot = Some(j);
+                break;
+            }
+        }
+        match pivot {
+            None => {
+                col_index += 1;
+                continue;
+            },
+            Some(jj) => {
+                if jj != row_index {
+                    for w in col_index/64..N {
+                        mat.rows[row_index][w] ^= mat.rows[jj][w];
+                    }
+                }
+                assert!(mat.rows[row_index].bit(col_index));
+                for j in row_index+1..M {
+                    if mat.rows[j].bit(col_index) {
+                        for w in col_index/64..N {
+                            mat.rows[j][w] ^= mat.rows[row_index][w];
+                        }
+                    }
+                }
+                row_index += 1;
+            }
+        }
+        col_index += 1;
+    }
     let rank = row_index as u32;
-
     return rank;
 }
--- four_russians/src/main.rs
+++ four_russians/src/main.rs
@@ -1,24 +1,86 @@
 mod arithmetic;
 mod four_russians;
 
-use crate::arithmetic::Word;
+use std::cmp;
 
-use crate::four_russians::Matrix;
-use crate::four_russians::Bit;
-use crate::four_russians::print_mat;
-use crate::four_russians::print_gf2_mat;
-use crate::four_russians::four_russians_mat_print;
-use crate::four_russians::four_russians_rank;
+use crate::arithmetic::Word;
+use crate::four_russians::{four_russians_mat_print, log_2, four_russians_rank, print_mat, print_gf2_mat, Matrix, Bit};
 
 
 fn main() {
-    let mut mat = Matrix::<Word, 2, 2>{
-        rows : [[1,2], [4,5]]
+    let mut mat = Matrix::<Word, 32, 2>{
+        rows : [[2,2], [4,5], [7, 28], [31, 13], [127, 5011], [3456, 255],
+                [123456, 78910], [666, 666666],
+                [11166744151617181920, 11121314847317181920],[11121314234517181920, 11123854151617181920],
+                [15536314151617181920, 12342344151617181920], [11121314198647181920, 11121314151901281920],
+                [17733314151617181920, 11121314151617181234], [11121334151617181920, 11958314151432081920],
+                [11121314151617181234, 11121314151617183253], [11122344151617181920, 11121314151689321920],
+                [11121314151617590420, 11123424151617181920], [11121314151695861920, 1112131415138581920],
+                [11121319090161718120, 16221314151617181920], [18390314151617181920, 11112344151617181920],
+                [11121312291617181920, 11121311230617181920], [12345313453617181920, 18341314151617181920],
+                [11121312201617181920, 11121314151612312920], [17121314151617181920, 11121323451617181920], 
+                [11121314151933381920, 11121314151349181920], [11125963151617181920, 11121314151619085920],
+                [11121313859617181920, 11289534151617181920], [11121393485617181920, 11121314151937581920],
+                [11132454151617181920, 11196744151617181920], [11121314169737181920, 11121314123487181920],
+                [11188844151617181920, 11139774151617181920], [11121314128357181920, 11121314151698524920]]
     };
-    print_mat(&mat);
+    let mut mat2 = Matrix::<Word, 32, 2>{
+        rows : [[0,0], [0,0], [0,0], [0,0], [0,0], [0,0],
+                [0,0], [0,0],
+                [0,0], [0,0],
+                [0,0], [0,0],
+                [0,0], [0,0],
+                [0,0], [0,0],
+                [0,0], [0,11121312291617181920],
+                [0,0], [0,11121312234598345340],
+                [0,0], [0,0],
+                [0,0], [0,0],
+                [0,0], [0,0],
+                [0,0], [0,0],
+                [0,0], [0,0],
+                [0,0], [0,0],]
+    };
+    println!("Matrix partitioned by word size:\n");
     print_gf2_mat(&mat);
-    four_russians_rank(&mut mat);
-    four_russians_mat_print(7, &mat);
 
-    println!("{}", format!("{:0>7}", format!("{:b}", mat.rows[0].bits(9*7, 7))));
+    /*
+    println!("0 bit of row 0 is nonzero: {:?}", mat.rows[0].bit(0));
+    println!("2 bit of row 1 is nonzero: {:?}", mat.rows[1].bit(2));
+    println!("1 bit of row 1 is nonzero: {:?}", mat.rows[1].bit(1));
+    println!("0 bit of row 1 is nonzero: {:?}", mat.rows[1].bit(0));
+    println!("63 bit of row 9 is nonzero: {:?}", mat.rows[9].bit(63));
+    println!("62 bit of row 9 is nonzero: {:?}", mat.rows[9].bit(62));
+
+    println!("First five bits of row 0 are: {:?}", mat.rows[0].bits(0, 7));
+    println!("First five bits of row 1 are: {:?}", mat.rows[1].bits(0, 7));
+    println!("First five bits of row 2 are: {:?}", mat.rows[2].bits(0, 7));
+    println!("First five bits of row 9 are: {:?}", mat.rows[9].bits(0, 7));
+
+    println!("Bits 62-65 of row 0 are: {:?}", mat.rows[0].bits(62, 4));
+    println!("Bits 62-65 of row 1 are: {:?}", mat.rows[1].bits(62, 4));
+    println!("Bits 62-65 of row 2 are: {:?}", mat.rows[2].bits(62, 4));
+    println!("Bits 62-65 of row 9 are: {:?}", mat.rows[9].bits(62, 4));
+    */
+
+    let rank = four_russians_rank(&mut mat);
+    println!("\nMatrix partitioned by four russian blocks has rank {:?}\n", rank);
+    let c = log_2(mat.num_rows().try_into().unwrap());
+    four_russians_mat_print(c, &mat);
+
+    if rank == mat.num_rows().try_into().unwrap() {
+        println!("\n The matrix is full rank!");
+    }
+
+    four_russians_mat_print(c, &mat2);
+    let rank2 = four_russians_rank(&mut mat2);
+    println!("\nMatrix partitioned by four russian blocks has rank {:?}\n", rank2);
+    four_russians_mat_print(c, &mat2);
+
+    if rank2 == mat2.num_rows().try_into().unwrap() {
+        println!("\n The matrix is full rank!");
+    }
+
+    //println!("{}", format!("{:0>c$}", format!("{:b}", mat.rows[0].bits(12*c, c))));
+    //println!("{}", format!("{:0>c$}", format!("{:b}", mat.rows[1].bits(12*c, c))));
+    //println!("{}", mat.rows[0].bit(0));
 }
